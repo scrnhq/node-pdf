303acf044307127c3b2988f32d808b37e7796ef5
Erik Gaal <me@erikgaal.nl>
2019-02-08 14:47:10 +0100

    Start basic express app

---
+++ app.js
@@ -0,0 +1,20 @@
+var createError = require('http-errors');
+var express = require('express');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+module.exports = app;
