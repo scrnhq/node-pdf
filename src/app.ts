55642211c062c8b150146094461b0d6988b017ab
Erik Gaal <me@erikgaal.nl>
2019-04-06 14:17:41 +0200

    Add welcome view

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import logger from 'morgan';
 import * as Sentry from '@sentry/node';
 import createError, { HttpError } from 'http-errors';
@@ -15,12 +16,19 @@ if (process.env.SENTRY_DSN) {
 
 app.use(Sentry.Handlers.requestHandler());
 
+app.set( "views", path.join( __dirname, "../views" ) );
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/pdf', pdfRouter);
 app.use('/screenshot', screenshotRouter);
+
+app.use('/', (req: Request, res: Response) => {
+  res.render('welcome');
+})
 
 // catch 404 and forward to error handler
 app.use((req: Request, res: Response, next: NextFunction) => {
